039613077f027930223570f9ea677f03d3688114
Juan Amiguet Vercher <jamiguet@gmail.com>
2017-10-18 09:28:16 +0200

    Fixes type of customized variable.

--- ja-network.el
+++ ja-network.el
@@ -41,7 +41,7 @@
 
 (defcustom ja-network-machine-interface-mapping ()
   "List of interfaces providing network per machine."
-  :type '(list (alist :key-type string :value-type (list string)))
+  :type '(list (cons string (cons string string)))
   :group 'network)
 
 (defcustom ja-network-update-time-interval 120
