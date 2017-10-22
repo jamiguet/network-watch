9537d492dbe8d29e6c8aa6fc7b13a0da45f744c3
Juan Amiguet Vercher <jamiguet@gmail.com>
2017-10-22 22:54:19 +0200

    Fixes uggly code in have-network-p

--- ja-network.el
+++ ja-network.el
@@ -3,10 +3,11 @@
      
 ;; Author: Juan Amiguet Vercher <jamiguet@gmail.com>
 ;; Created: 17 Oct 2017
-;; Version: 1.0
+;; Version:
+;; Package-Version: 1.0
 ;; Keywords: unix tools hardware lisp
 ;; Homepage: https://github.com/jamiguet/ja-network
-;; Package-Requires: ((org "20171016"))
+;; Package-Requires: ((org "9.1") (emacs "24.3"))
 
 ;; This file is not part of GNU Emacs.
 
@@ -14,13 +15,65 @@
 
 
 ;;; Commentary:
-;;; Adds support for intermitent netowrk connectivity.
-;;; Through two hooks run when network connectivity appears and
-;;; disappears.
+
+;; # ja-network
+;; 
+;; Emacs library for handling intermitent network access.  It provides
+;; two hooks *network-up-hook* and *network-down-hook* every
+;; *network-update-time-interval* the network status is checked if
+;; nothing changed since the previous time no hooks are invoked.  If
+;; access to a network is possible then the *network-up-hook* is run.
+;; Conversely when network connectivity is lost the *network-down-hook*
+;; is run.
+;; 
+;; The library is designed to support multiple machines on the same
+;; customised variables setup.
+;; I share all my set-up accross diefferent machines.
+;; The network interfaces of any unknown machines are
+;; automatically added to the customised variable
+;; *machine-interface-mapping* which is part of the *network* group.
+;; 
+;; 
+;; ## Setup
+;; 
+;; The first time the library is used on a computer it will add the
+;; machine name and the set of all active interfaces to the
+;; *machine-interface-mapping* customised variable.
+;; Edit the variable
+;; by hand to remove any loopback interfacess or to add the name of
+;; any interfaces which are not connected.
+;; 
+;; You can also adapt the *network update-time-interval* to your liking.
+;; The *have-network-hook* is run at the end of Emacs startup if you have
+;; connectivity.
+;; 
+;; 
+;; ## Utility function
+;; 
+;; Besides the two hooks the library also provides a *have-network-p*
+;; function which returns not nil when a listed interface is up.
+;; 
+;; 
+;; ## Example
+;; 
+;; In this example *gmail-notifier* is configured with the help of
+;; *ja-network* it is automatically started and stopped when the network
+;; 	is up or down respectively.
+;; 
+;; 	(require 'ja-network)
+;; 	(require 'gmail-notifier)
+;; 	
+;; 	(setq gmail-notifier-username "jamiguet")
+;; 	(setq gmail-notifier-password ja-password)
+;; 
+;;      (add-hook 'network-up-hook 'gmail-notifier-start)
+;; 	g(add-hook 'network-down-hook 'gmail-notifier-stop)
+;; 
 ;; 
 
 ;;; Code:
 (require 'org)
+(require 'cl-lib)
 
 (defvar network-up-hook)
 (defvar network-down-hook)
@@ -31,7 +84,7 @@
 (setq machine-name (substring (eshell-command-result "hostname") 0 -1 ))
 
 ;; TODO:
-;; Publish on melpa.
+;; Convert into a global-minor-mode
 
 ;; Define base group for network information
 (defgroup network nil
@@ -45,7 +98,7 @@
   :group 'network)
 
 (defcustom ja-network-update-time-interval 120
-  "Refersh reate for network statatus."
+  "Refersh reate for network status."
   :type 'integer
   :group 'network)
 
@@ -62,19 +115,16 @@
 
 
 
-
 (defun ja-network-have-network-p ()
   "Return nil if there are no active network interfaces."
   (catch 'break
-    (dolist (intf
-	     (cadr (assoc machine-name ja-network-machine-interface-mapping))
-	     value)
-      (if (assoc intf (network-interface-list))
-	  (progn (setq value t)
-		 (throw 'break value))
-	(setq value nil)))
-    ))
-
+    (let (value)
+      (dolist (intf
+	       (cadr (assoc machine-name ja-network-machine-interface-mapping)))
+	(setq value (cl-find intf (cl-mapcar #'car (network-interface-list)) :test #'string= ) )
+	(if value
+	    (throw 'break value)))
+      )))
 
 
 (defun ja-network-update-system-state ()
