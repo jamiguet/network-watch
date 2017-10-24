2b48c765bd2d33099c92ee2c64eb581a211407d4
Juan Amiguet Vercher <jamiguet@gmail.com>
2017-10-24 09:43:52 +0200

    Converting to a global minor mode.

--- README.md
+++ README.md
@@ -1,52 +1,56 @@
-# network-mode
+# network-watch
 
-Emacs library for handling intermitent network access.  It provides
+Emacs global minormode for handling intermitent network access.  It provides
 two hooks *network-up-hook* and *network-down-hook* every
-*network-update-time-interval* the network status is checked if
+*network-watch-time-interval* the network status is checked if
 nothing changed since the previous time no hooks are invoked.  If
 access to a network is possible then the *network-up-hook* is run.
 Conversely when network connectivity is lost the *network-down-hook*
 is run.
 
-The library is designed to support multiple machines on the same
-customised variables setup. I share all my set-up accross diefferent
-machines. The network interfaces of any unknown machines are
+The minor mode is designed to support multiple machines on the same
+customised variables setup.
+I share all my set-up accross diefferent machines.
+The network interfaces of any unknown machines are
 automatically added to the customised variable
-*machine-interface-mapping* which is part of the *network* group.
+*network-watch-interface-mapping* which is part of the *network*
+customisation group.
 
 
 ## Setup
 
 The first time the library is used on a computer it will add the
 machine name and the set of all active interfaces to the
-*machine-interface-mapping* customised variable. Edit the variable by
-hand to remove any loopback interfacess or to add the name of any
-interfaces which are not connected.
+*network-watch-interface-mapping* customised variable.
+Edit the variable by hand to remove any loopback interfacess
+or to add the name of any interfaces which are not connected.
 
-You can also adapt the *network update-time-interval* to your liking.
-The *have-network-hook* is run at the end of emacs startup if you have
+You can also adapt the *network-watch-update-time-interval* to your liking.
+The *have-network-hook* is run at the end of Emacs startup if you have
 connectivity.
 
 
 ## Utility function
 
-Besides the two hooks the library also provides a *have-network-p*
+Besides the two hooks the library also provides a *network-watch-p*
 function which returns not nil when a listed interface is up.
-
 
 
 ## Example
 
 In this example *gmail-notifier* is configured with the help of
-*network-mode* it is automatically started and stopped when the network
+*network-watch* it is automatically started and stopped when the network
 	is up or down respectively.
 
-	(require 'network-mode)
+	(require 'network-watch)
 	(require 'gmail-notifier)
 	
 	(setq gmail-notifier-username "jamiguet")
 	(setq gmail-notifier-password ja-password)
 
-	(add-hook 'network-up-hook 'gmail-notifier-start) 
-	(add-hook 'network-down-hook 'gmail-notifier-stop) 
+     (add-hook 'network-up-hook 'gmail-notifier-start)
+     (add-hook 'network-down-hook 'gmail-notifier-stop)
+
+
+
 
--- network-watch.el
+++ network-watch.el
@@ -1,5 +1,5 @@
-;;; network-mode.el --- Support for intermitent network connectivity
-;; Copyright (C) 2010-2017 Your Name
+;;; network-watch.el --- Support for intermitent network connectivity
+;; Copyright (C) 2010-2017 Juan Amiguet Vercher
      
 ;; Author: Juan Amiguet Vercher <jamiguet@gmail.com>
 ;; Created: 17 Oct 2017
@@ -15,50 +15,51 @@
 
 ;;; Commentary:
 
-;; # network-mode
+;; # network-watch
 ;; 
-;; Emacs library for handling intermitent network access.  It provides
+;; Emacs global minormode for handling intermitent network access.  It provides
 ;; two hooks *network-up-hook* and *network-down-hook* every
-;; *network-update-time-interval* the network status is checked if
+;; *network-watch-time-interval* the network status is checked if
 ;; nothing changed since the previous time no hooks are invoked.  If
 ;; access to a network is possible then the *network-up-hook* is run.
 ;; Conversely when network connectivity is lost the *network-down-hook*
 ;; is run.
 ;; 
-;; The library is designed to support multiple machines on the same
+;; The minor mode is designed to support multiple machines on the same
 ;; customised variables setup.
 ;; I share all my set-up accross diefferent machines.
 ;; The network interfaces of any unknown machines are
 ;; automatically added to the customised variable
-;; *machine-interface-mapping* which is part of the *network* group.
+;; *network-watch-interface-mapping* which is part of the *network*
+;; customisation group.
 ;; 
 ;; 
 ;; ## Setup
 ;; 
 ;; The first time the library is used on a computer it will add the
 ;; machine name and the set of all active interfaces to the
-;; *machine-interface-mapping* customised variable.
+;; *network-watch-interface-mapping* customised variable.
 ;; Edit the variable by hand to remove any loopback interfacess
 ;; or to add the name of any interfaces which are not connected.
 ;; 
-;; You can also adapt the *network update-time-interval* to your liking.
+;; You can also adapt the *network-watch-update-time-interval* to your liking.
 ;; The *have-network-hook* is run at the end of Emacs startup if you have
 ;; connectivity.
 ;; 
 ;; 
 ;; ## Utility function
 ;; 
-;; Besides the two hooks the library also provides a *network-p*
+;; Besides the two hooks the library also provides a *network-watch-p*
 ;; function which returns not nil when a listed interface is up.
 ;; 
 ;; 
 ;; ## Example
 ;; 
 ;; In this example *gmail-notifier* is configured with the help of
-;; *ja-network* it is automatically started and stopped when the network
+;; *network-watch* it is automatically started and stopped when the network
 ;; 	is up or down respectively.
 ;; 
-;; 	(require 'network-mode)
+;; 	(require 'network-watch)
 ;; 	(require 'gmail-notifier)
 ;; 	
 ;; 	(setq gmail-notifier-username "jamiguet")
@@ -75,6 +76,7 @@
 (defvar network-up-hook)
 (defvar network-down-hook)
 (defvar machine-name)
+(defvar network-watch)
 
 ;; define that we are on a specific machine
 (setq machine-name (substring (eshell-command-result "hostname") 0 -1 ))
@@ -82,48 +84,49 @@
 ;; Define base group for network information
 (defgroup network nil
   "Customisation group for network availability hooks"
-  :group 'Emacs)
+  :group 'Communication)
 
 
-(define-minor-mode network-mode
+(define-minor-mode network-watch
   "Network is automatically on when there is a valid network
 interface active."
-      ;; The network-initial value.
-      :network-init-value nil
-      ;; The indicator for the mode line.
-      :lighter " (N)"
-      :group 'network)
+  :initial-value t
+  ;; The indicator for the mode line.
+  :global t
+  :variable (  '(network-watch-p) .  network-watch-update-wrap )
+  :require 'network-watch
+  :group 'network)
 
 
-(defcustom network-machine-interface-mapping ()
+(defcustom network-watch-interface-mapping ()
   "List of interfaces providing network per machine."
   :type 'sexp
   :group 'network)
 
-(defcustom network-update-time-interval 120
+(defcustom network-watch-time-interval 120
   "Refersh reate for network status."
   :type 'integer
   :group 'network)
 
 
-(defun network-machine-add ()
+(defun network-watch-add-machine ()
   "Add the current machine to the machine interface mapping."
   (defvar intf-names)
   (setq intf-names ())
   (dolist (intf (network-interface-list) intf-names)
     (setq intf-names (cons (car intf) intf-names)))
-  (customize-save-variable 'network-machine-interface-mapping
-		(cons (list  machine-name intf-names ) network-machine-interface-mapping)
+  (customize-save-variable 'network-watch-interface-mapping
+		(cons (list  machine-name intf-names ) network-watch-interface-mapping)
 		"List of machine interfaces."))
 
 
 
-(defun network-p ()
+(defun network-watch-p ()
   "Return nil if there are no active network interfaces."
   (catch 'break
     (let (value)
       (dolist (intf
-	       (cadr (assoc machine-name network-machine-interface-mapping)))
+	       (cadr (assoc machine-name network-watch-interface-mapping)))
 	(setq value
 	      (cl-find intf
 		       (cl-mapcar #'car (network-interface-list))
@@ -133,39 +136,51 @@ interface active."
       )))
 
 
-(defun network-update-system-state ()
+(defun network-watch-update-system-state ()
   "Internal method update the network state variable."
-  (setq network-mode (network-p))
+  (setq network-watch (network-watch-p))
+  ;; toggle the state of the global mode also
   )
 
 
-(defun network-update-state ()
+(defun network-watch-update-wrap (state)
+  "Blatantly ignore the set state as it is managed by the interface."
+  (network-watch-update-state))
+
+
+(defun network-watch-update-state ()
   "Run hooks only on network status change."
-  (if (cl-set-exclusive-or (network-p) network-mode)
+  (if (cl-set-exclusive-or
+       (if (listp (network-watch-p))
+	   (network-watch-p)
+	 (list (network-watch-p)))
+       (if (listp network-watch)
+	   network-watch
+	 (list network-watch)))
       (progn
-	(if (network-p) (run-hooks 'network-up-hook)
+	(if (network-watch-p) (run-hooks 'network-up-hook)
 	  (run-hooks 'network-down-hook))
-	(network-update-system-state)
+	(network-watch-update-system-state)
 	))
-  (run-with-timer network-update-time-interval  nil 'network-update-state)
+  (run-with-timer network-watch-time-interval  nil 'network-watch-update-state)
   (message "Network state update")
   )
 
 
-(defun network-init ()
-  "Network-Initialises the inner state of the network and configures machine if neccessary."
-  (if (not (assoc machine-name network-machine-interface-mapping))
+(defun network-watch-init ()
+  "Network-Watch-Initialises the inner state of the network and configures machine if neccessary."
+  (if (not (assoc machine-name network-watch-interface-mapping))
       (progn
 	(message "Machine not pressent adding machine. Customise network group if needed.")
-	(network-machine-add)))
-  (network-update-system-state)
-  (run-with-timer network-update-time-interval  nil 'network-update-state)
-  (if (network-p) (run-hooks 'network-up-hook))
+	(network-watch-add-machine)))
+  (network-watch-update-system-state)
+  (run-with-timer network-watch-time-interval  nil 'network-watch-update-state)
+  (if (network-watch-p) (run-hooks 'network-up-hook))
   (message "Network init")
   )
 
 
-(add-hook 'after-network-network-init-hook 'network-init)
+(add-hook 'after-init-hook 'network-watch-init)
 
-(provide 'network-mode)
-;;; network-mode.el ends here
+(provide 'network-watch)
+;;; network-watch.el ends here
